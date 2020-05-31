b4b2aa57639796fead8793171aea23d4d3ee7693
alisajadih <alisajadih@gmail.com>
2020-06-01 02:27:23 +0430

    idk

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// const blogContent = document.querySelectorAll(".blog-content");
+// const blogContentArr = Array.from(blogContent);
+// const maxHeight = blogContentArr.reduce((prev, acc) => {
+//   console.log(acc.querySelector('.text').style);
+//   return 
+// }, 0);
