be981ed6d4f60024ab75598781f66c254ed5924a
alisajadih <alisajadih@gmail.com>
2020-08-20 23:18:00 +0430

    init nextjs

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-// const blogContent = document.querySelectorAll(".blog-content");
-// const blogContentArr = Array.from(blogContent);
-// const maxHeight = blogContentArr.reduce((prev, acc) => {
-//   console.log(acc.querySelector('.text').style);
-//   return 
-// }, 0);
